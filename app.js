9b81e5b6b3ff5d9dfc315e6b7d3bb2a03ae472b1
Maximillionair <maxmillian.heim@outlook.com>
2025-03-14 12:12:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3500;
   } catch (err) {
     console.log('connect failed, error:', err);
     prosses.exit(1);
-
+  };
 // register view engine
 app.set('view engine', 'ejs');
 
