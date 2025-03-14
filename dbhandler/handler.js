002cf9fcaa03ecb6f322cf0f0a8e181d8f8552a5
Maximillionair <maxmillian.heim@outlook.com>
2025-03-14 13:02:07 +0100

    feil ip

--- dbhandler/handler.js
+++ dbhandler/handler.js
@@ -7,7 +7,7 @@ async function connectDB() {
         console.log("MongoDB connected successfully!");
     });
 
-      await mongoose.connect('mongodb://10.12.10.240');
+      await mongoose.connect('mongodb://10.12.10.111');
       console.log('Connected to MongoDB');
 
     } catch (error) {
