dc1c3fb48ddfd7f25b2af5d8379133d5811d5dd0
Maximillionair <maxmillian.heim@outlook.com>
2025-03-18 09:32:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -31,10 +31,6 @@ app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
 
-app.get('/auth', (req, res) => {
-  res.render('register');
-});
-
 // blog routes
 app.use('/blogs', blogRoutes);
 app.use("/auth", authroutes)
